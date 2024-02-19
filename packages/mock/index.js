03f673c2ae88477854698496a5c02a4c0c3de292
awen <71752352@qq.com>
2024-02-19 23:37:26 +0800

    axios ok

--- packages/mock/index.js
+++ packages/mock/index.js
@@ -1,31 +1,30 @@
-const Config = {
-  mockDir:'api',
-  baseUrl:'/api',
-}
-const jsonImporters = import.meta.glob('@/<%=(option.mockDir||"api")%>/*.json')
-console.log(jsonImporters)
-for(var k in jsonImporters){
-  const regs = k.match(/\W([a-zA-Z]+\w+)\.json$/i)
-  if(regs&&regs.length>1){
-    const route = regs[1]
-    console.log(route)
-  }
-}
+/**
+ * 基于mockjs的前端api拦截器，模板使用参考https://github.com/nuysoft/Mock/wiki
+ */
+import Mockjs from "mockjs"
+const initMocks = import.meta.glob('@/<%=(option.mockDir||"mock")%>/*.js',{ import: 'default',eager: true})
+console.log(initMocks)
 
-// 封装@vitescv/axios 自动处理token
-function interceptorAxios(axios){
-  // 添加请求拦截器
-  axios.interceptors.request.use(function (config) {
-    config.headers['x-mock'] =  true
-    return config;
+export default function(config){
+  config = Object.assign({
+    mockDir:'mock',        //mockjs的template目录
+    timeout:"500-1500"
+  },config)
+  Mockjs.setup({
+    timeout:config.timeout
   })
-}
-
-export default function(config,Context){
-  Object.assign(Config,config)
-  if(!this.axios){
-    console.error('[@vitescv/mock] @vitescv/axios must install and init!')
-    return
-  }
-  interceptorAxios(Context.axios)
+  Object.values(initMocks).forEach(initMock=>{
+    const mockDatas = initMock(Mockjs)
+    if(Array.isArray(mockDatas)){
+      mockDatas.forEach(data=>{
+        if(data.rurl&&data.template){
+          if(data.rtype){
+            Mockjs.mock(data.rurl,data.rtype,data.template)
+          }else{
+            Mockjs.mock(data.rurl,data.template)
+          }
+        }
+      })
+    }
+  })
 }
---
+++ packages/mock/readme.md
@@ -0,0 +1,69 @@
+### 简介
+
+vitescv的mockjs模块， 用于在前端直接拦截请求（`XMLHttpRequest`,`axios`,`fetch`）生成json数据，请注意打包的时候如果不需要要去掉这个模块，否则会一起打包
+
+ps:特别适合前端直接调试开发，和打演示demo
+
+### 安装
+
+```js
+pnpm add @vitescv/mock
+```
+
+### 使用
+
+```js
+#在config.js中配置默认属性
+modules:{
+  ...
+  "@vitescv/mock":{
+    timeout:'500-1500',        //参考mockjs的setup的timeout参数
+    mockDir:'mock'             //mock模板路由文件的文件夹
+  },
+}
+```
+
+#### 路由文件
+
+模块会自动加载`mockDir`中的js文件，该文件为路由文件，返回一个默认初始化函数，改函数返回一个`route`对象数组,包含以下几个属性：
+
+**rurl** 拦截路由的url，必须。    *参考`Mock.mock()`的第一个参数*
+**rtype** 拦截的请求方法，可选。  *参考`Mock.mock()`的第二个参数*
+**data** 数据,必填。             *参考`Mock.mock()`的第三个参数*
+
+```
+# mock/users.js
+export default function(Mock){
+  return [
+    {
+      rurl:'/api/users',
+      rtype:'GET',
+      data:{
+        code:1,
+        msg:'success',
+        'data|10':{
+          'id|+1':1,
+          name:'@name',
+          'age|18-55':1,
+          'sex|0-1':1,
+          email:'@email',
+          phone:'@phone',
+          address:'@area',
+          cover:Mock.Random.dataImage( '250x250','@word'),
+          remark:Mock.Random.paragraph(10,50),
+          createDate:'@datetime',
+        }
+      }
+    },{
+      rurl:'/api/funtest',
+      data:function(req){
+        return {
+          title:'Function Test',
+          info:'this is a function template'
+        }
+      }
+    }
+  ]
+}
+
+```
