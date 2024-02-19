da45f4269ac32e7feedf7db87cfdbd0821da95c2
awen <71752352@qq.com>
2024-02-19 17:56:08 +0800

    axios初始化在模块初始化时完成，提到app init初始化之前

--- packages/axios/index.js
+++ packages/axios/index.js
@@ -5,15 +5,10 @@ import axios from "axios"
 
 export default function(config,Context){
   const instance = axios.create(config)
-  Context.hook("APP:INIT",function(options) {
-    // 💡对外公开提供全局方法
-    Context.axios = instance
-    Object.defineProperty(Context.Vue.prototype,'$axios',{
-      get(){
-        return instance
-      }
-    })
+  Context.axios = instance
+  Object.defineProperty(Context.Vue.prototype,'$axios',{
+    get(){
+      return instance
+    }
   })
-  // Context.hook("APP:CREATED",function() {
-  // })
 }
